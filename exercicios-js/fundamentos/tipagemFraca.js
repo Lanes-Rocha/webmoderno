127dbfe9b7ac3ef8235c97887477bedddee619ae
Lanes Rocha <lanes.rocha@admin.uvv>
2020-12-08 21:16:18 -0300

    Criação do arquivo tipagemFraca.js

---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+//Uma variável no javascript pode ter muitos tipos de valores, pois é uma linguagem de tipagem fraca.
+//Abaixo temos alguns exemplos
+
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer); //imprimindo o tipo da variável
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer);
