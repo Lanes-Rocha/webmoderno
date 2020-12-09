ec4f406386209de49cd5f526ee11ea111d4a0a2f
Lanes Rocha <lanes.rocha@admin.uvv>
2020-12-09 20:17:22 -0300

    Criação do arquivo numerosAlgunsCuidados. js Formas de utilização que deve ser evitada.

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0);
+console.log("10" / 2); // no javascript essa string seria convertida em Number.
+console.log("show" * 2 ); //Not a Number
+console.log(0.1 + 0.7); //Imprecisão
+//console.log(10.toString()); não irá funcionar.
+console.log((10.345).toFixed(2));
