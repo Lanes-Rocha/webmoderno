01452120d7217babc67d9163354fab6488d2280a
Lanes Rocha <lanes.rocha@admin.uvv>
2020-12-09 22:18:08 -0300

    criação do arquivo string.js

---
+++ exercicios-js/fundamentos/strings.js
@@ -0,0 +1,9 @@
+const escola = "Universidade de Vila Velha";
+
+
+console.log(escola.charAt(4));// charAT mostra a posição da string.
+console.log(escola.charAt(32));// não vai retornar nada, pois não foi encontrado nenhum ccaractere nessa posição.
+console.log(escola.charCodeAt(3));// O valor a ser mostrado será o cod da tabela ASCII ou da CodeAt do caractere na posição 3.
+console.log(escola.indexOf('n'));//irá procurar se existe esse caractere e mostrar seu índice.
+console.log(escola.substring(1));//Não irá mostrar o caractere do índice 1 porém todo o resto sim
+console.log(escola.substring(0, 3));// irá mostrar os caracteres dos índices de 0 á 2, porém o 3 não
