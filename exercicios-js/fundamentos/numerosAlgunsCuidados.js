794b61d20091a5ca3e97a3e224aa82570774bf34
Lanes Rocha <lanes.rocha@admin.uvv>
2020-12-09 22:33:36 -0300

    Atualizações finais sobre strings

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7 / 0);
 console.log("10" / 2); // no javascript essa string seria convertida em Number.
+console.log('3' + 2);// irá concatenar pois o + tem sentido no contexto de string para concateções já outros operdores matemáticos não
 console.log("show" * 2 ); //Not a Number
 console.log(0.1 + 0.7); //Imprecisão
 //console.log(10.toString()); não irá funcionar.
--- exercicios-js/fundamentos/strings.js
+++ exercicios-js/fundamentos/strings.js
@@ -7,3 +7,15 @@ console.log(escola.charCodeAt(3));// O valor a ser mostrado será o cod da tabel
 console.log(escola.indexOf('n'));//irá procurar se existe esse caractere e mostrar seu índice.
 console.log(escola.substring(1));//Não irá mostrar o caractere do índice 1 porém todo o resto sim
 console.log(escola.substring(0, 3));// irá mostrar os caracteres dos índices de 0 á 2, porém o 3 não
+console.log('Escola '.concat(escola).concat("!!!"));// o .concat concatena 
+console.log(escola.replace("e", '3'));
+
+console.log('Ana,Maria,Lanes'.split(',')); //.split faz essa expressão virar array.
+
+
+
+
+
+
+console.log();
+console.log();
