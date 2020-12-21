0174a15f77f7efe975863d930a119bdf007b35b9
Lanes Rocha <lanes.rocha@hotmail.com>
2020-12-21 19:17:08 -0300

    Modicação no arquivo tiposArray.js

--- exercicios-js/fundamentos/tiposArray.js
+++ exercicios-js/fundamentos/tiposArray.js
@@ -1,1 +1,3 @@
-console.log("Criação do arquivo tiposArray.js")
+const valores = [7.7, 8.9, 6.3, 9.2];
+console.log(valores[0], valores[3]);
+console.log(valores[4]);// Em Javascript não aparece erro por tentar acessar um elemento no vetor que não existe;
