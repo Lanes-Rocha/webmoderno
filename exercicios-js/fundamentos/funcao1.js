5612698425a7183d709509b47b8cc53fc09db531
lanes.rocha <lanes.rocha@hotmail.com>
2021-03-04 21:16:55 -0300

    Atualização do arquivo funcao1.js

---
+++ exercicios-js/fundamentos/funcao1.js
@@ -0,0 +1,15 @@
+//FUNÇÃO SEM RETORNO
+
+function imprimirSoma(a, b)
+{
+    console.log(a+b);
+}
+imprimirSoma(2,3);
+
+//FUNÇÃO COM RETORNO
+
+function soma(a, b = 1) {
+    return a + b;
+}
+console.log(soma(2,3));
+console.log(soma(2));//Como temos o valor padrão de b = 1 quando imprimir a função o resultado será 3.
