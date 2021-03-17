cd5f02956931647534edaadabd7d71f4de67c6da
Lanes Rocha <lanes.rocha@hotmail.com>
2021-03-17 19:21:24 -0300

    atualização

--- exercicios-js/fundamentos/funcao1.js
+++ exercicios-js/fundamentos/funcao1.js
@@ -13,3 +13,4 @@ function soma(a, b = 1) {
 }
 console.log(soma(2,3));
 console.log(soma(2));//Como temos o valor padrão de b = 1 quando imprimir a função o resultado será 3.
+console.log(3)//Undefined
