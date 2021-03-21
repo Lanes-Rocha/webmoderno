13734d94a9a5c598339ce14d373a0ac3e09edf61
Lanes Rocha <lanes.rocha@hotmail.com>
2021-03-21 00:34:55 -0300

    Finalizando a aula de Var1

---
+++ exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{{{{
+// Embora a variável tenha sido declarada utilizando var dentro de um bloco
+// a mesma poderá ser chamada em qualquer lugar de seu código
+     var sera = 'Será??'
+     console.log(sera); 
+}}}}
+// Nesse caso como a variável foi criada fora de uma função ela poderá ser chamada em qualquer lugar
+console.log(sera);
+
+// Em um novo escopo especial, neste caso, uma função.
+// A variavel poderá ser utilizada e/ou estará visível apenas dentro da função
+
+function teste(){
+    var local = 123;
+    console.log(local);
+}
+// Abaixo a função a variável nao poderá ser utilizada, pois está dentro de um escopo de uma função.
+
+teste();// Chmando a função funcionará pois ela irá trazer seu escopo completo.
+console.log(local);// Ocorrerá um erro, pois a variável local, não poderá ser acessada.
