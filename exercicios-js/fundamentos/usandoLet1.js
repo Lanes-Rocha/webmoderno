4e3f31c01d0705d1ba2b9d1fc63e6518361ea9f3
Lanes Rocha <lanes.rocha@hotmail.com>
2021-03-23 17:15:17 -0300

    arquivo usandoLet1.js finalizado

--- exercicios-js/fundamentos/usandoLet1.js
+++ exercicios-js/fundamentos/usandoLet1.js
@@ -4,3 +4,10 @@ console.log("Começando com o Let");
 // * Escopo global
 // * Escopo de função
 // * Escopo de bloco
+
+var numero = 1;
+{
+    let numero = 2;
+    console.log('dentro = ', numero);
+}
+console.log('fora = ', numero);
