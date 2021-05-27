df0260bc651e73df4c9d8715ba8218259f3975fd
Lanes Rocha <lanes.rocha@hotmail.com>
2021-05-26 22:24:47 -0300

    criação do arquivo usandoVarEmLoop.js

---
+++ exercicios-js/fundamentos/usandoVarEmLoop.js
@@ -0,0 +1,11 @@
+var funcs = [];
+
+for(var i=0; i < 10; i++){ 
+    funcs.push(function(){
+        console.log(i);   
+    })
+}
+// como car não tem escopo o retorno das funções trará o mesmo valor...
+// ou seja todos os valores serão 10 abaixo
+funcs[2](); 
+funcs[8]();
