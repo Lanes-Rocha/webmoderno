b51922227edfc5f0c920f52ab906b019af4f2ce9
Lanes Rocha <lanes.rocha@hotmail.com>
2021-12-08 22:01:17 -0300

    Finalizando os conceitos de operadores lógicos.

--- exercicios-js/fundamentos/logicos.js
+++ exercicios-js/fundamentos/logicos.js
@@ -1,1 +1,17 @@
-console.log('ola testando a sincronização');
+function compras(trabalho1, trabalho2){
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTV50 = trabalho1 && trabalho2
+    //const comprarTV32 = !!(trabalho1 ^ trabalho2) //bitwise xor
+    const comprarTV32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete //operador unário negação lógica
+
+    //criação do objeto chave e valor neste caso precisamos colocar chave e valor, pois da forma
+    //descrita abaixo já faz de forma ocutal. ex.: comprarSorvete:comprarSoverte entao apenas declarando
+    //da forma abaixo, como citado, nosso objeto já faz de forma oculta para não haver redundância.
+    return { comprarSorvete, comprarTV50, comprarTV32, manterSaudavel} 
+
+}
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
