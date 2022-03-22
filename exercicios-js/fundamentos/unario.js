153ecf2e02a6ec0d51d1b9f3fa77f9a9c057b3be
Lanes <lanes.rocha@hotmail.com>
2022-03-22 01:25:34 -0300

    unario e ternario

---
+++ exercicios-js/fundamentos/unario.js
@@ -0,0 +1,14 @@
+let num1 = 1
+let num2 = 2
+
+// forma pós fixado
+num1++ 
+console.log(num1)
+
+//forma prefixada
+--num1
+console.log(num1) // volta a valer 1
+
+//==============================//
+
+console.log(++num1 === num2--)
