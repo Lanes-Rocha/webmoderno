18595a43a2deeeaef5b9774992f7be39f10c709f
Lanes Rocha <lanes.rocha@hotmail.com>
2022-05-06 21:33:36 -0300

    Conceitos sobre a função arrow

---
+++ exercicios-js/funcoes/arrowFunction.js
@@ -0,0 +1,12 @@
+let dobro = function(){
+    return 2 * a
+}
+
+//DIFERENÇA UTILIZANDO A FUNÇÃO Arrow
+
+dobro = (a) =>{
+    return 2 * a
+}
+
+dobro = a => 2 * a // return implícito
+console.log(dobro(ma.PI)) // irá imprimir o dobro de PI
