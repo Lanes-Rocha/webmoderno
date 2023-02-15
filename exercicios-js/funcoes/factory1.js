a0882e3d4d6d03de67167dd0c94018a0e49a7e6e
Lanes Rocha <lanes.rocha@hotmail.com>
2023-02-15 18:28:09 -0300

    arquivo finalizado

---
+++ exercicios-js/funcoes/factory1.js
@@ -0,0 +1,10 @@
+// Factory simples é uma tipo de função que retorna um novo objeto no final
+
+function criarPessoa(){
+    return{
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+
+}
+console.log(criarPessoa())
